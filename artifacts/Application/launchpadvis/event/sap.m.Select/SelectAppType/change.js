d7e9cd22538035a2ac9701824724222cc5c67998
Admin <terryaugenikos.2000@gmail.com>
2024-08-07 13:33:09 +0000

    Image swap bug

--- artifacts/Application/launchpadvis/event/sap.m.Select/SelectAppType/change.js
+++ artifacts/Application/launchpadvis/event/sap.m.Select/SelectAppType/change.js
@@ -1,13 +1,17 @@
 const id = NodeID.getText();
 const node = graph.getCellById(id);
 const appType = SelectAppType.getSelectedKey();
+let icon;
 
 if (appType === "adaptive") {
-    node.attr("icon/xlinkHref",`/public/images/platform/bare/${Init.systemTheme}/adaptive-app-designer.svg`);
+    icon = `/public/images/platform/bare/${Init.systemTheme}/adaptive-app-designer.svg`;
+    node.attr("icon/xlinkHref",icon);
 } else if (appType === "webapp") {
-    node.attr("icon/xlinkHref",`/public/images/platform/bare/${Init.systemTheme}/app-editor.svg`);
+    icon = `/public/images/platform/bare/${Init.systemTheme}/app-editor.svg`;
+    node.attr("icon/xlinkHref",icon);
 } else {
-    node.attr("icon/xlinkHref",`/public/images/platform/bare/${Init.systemTheme}/app-designer.svg`);
+    icon = `/public/images/platform/bare/${Init.systemTheme}/app-designer.svg`;
+    node.attr("icon/xlinkHref",icon);
 }
 
 node.attr("title/text", null);
@@ -15,5 +19,6 @@ node.attr("metadata/name", null);
 node.attr("metadata/appType", appType);
 node.attr("metadata/artifactID", null);
 modelSelectedNode.getData().name = null;
+modelSelectedNode.getData().icon = icon;
 modelSelectedNode.refresh();
 
